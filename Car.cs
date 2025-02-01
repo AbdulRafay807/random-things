4f85a47b1e6bdf6e9b8f560fcad2992f24f46770
rafeyismail <lucyelizabeth831@gmail.com>
2025-02-01 19:34:34 +0500

    try something random

---
+++ Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+  public class  Car
+    {
+              
+        public void Wheel()
+        {
+            Console.WriteLine("Wheel is Running");
+        }
+
+        public void engine()
+        {
+            Console.WriteLine("Enigine is start");
+        }
+
+
+        public Car( int car_number)
+        {
+            Console.WriteLine(car_number);
+        }
+
+        public Car()
+        {
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using method;
+
+
+//Car running = new Car();
+//running.Wheel();
+//running.engine();
+
+Car number_car = new Car(543645);
+Car number_car2 = new Car();
